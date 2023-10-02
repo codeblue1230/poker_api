5c2dbce24a038263b9090c94de4f73778eef6dcc
Marc Engle <marcengle30@gmail.com>
2023-10-02 11:03:09 -0400

    fixed run.py

--- run.py
+++ run.py
@@ -1,3 +1,3 @@
-from poker_inventory import Poker
+from poker_inventory import Poker, api
 
-app = Poker()
+my_app=api.add_resource(Poker, "/<string:hole1>_<string:hole2>/<string:flop1>_<string:flop2>_<string:flop3>_<string:turn>_<string:river>")
