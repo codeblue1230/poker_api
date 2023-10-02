c6ee458bb0fc69ad2d54413a6d67933786be47ce
Marc Engle <marcengle30@gmail.com>
2023-10-02 11:50:14 -0400

    undid hosting changes

--- poker_inventory/__init__.py
+++ poker_inventory/__init__.py
@@ -385,4 +385,4 @@ class Poker(Resource):
         except:
             abort(400, error="Invalid Input")
     
-# api.add_resource(Poker, "/<string:hole1>_<string:hole2>/<string:flop1>_<string:flop2>_<string:flop3>_<string:turn>_<string:river>")
+api.add_resource(Poker, "/<string:hole1>_<string:hole2>/<string:flop1>_<string:flop2>_<string:flop3>_<string:turn>_<string:river>")
--- run.py
+++
@@ -1,8 +0,0 @@
-from poker_inventory import Poker, api
-import requests
-
-BASE = "https://best-hold-em-hand.onrender.com"
-
-my_app=api.add_resource(Poker, "/<string:hole1>_<string:hole2>/<string:flop1>_<string:flop2>_<string:flop3>_<string:turn>_<string:river>")
-
-response = requests.get(f"{BASE}/2h_5s/2c_Jd_7c_5h_8d")
