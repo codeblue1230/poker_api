edde229b433eb26e08aaa2e1cfec1cd30e85dc27
Marc Engle <marcengle30@gmail.com>
2023-10-07 17:37:28 -0400

    added CORS

--- poker_inventory/__init__.py
+++ poker_inventory/__init__.py
@@ -1,7 +1,9 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, render_template
 from flask_restful import Api, Resource, abort
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 # Landing Page
